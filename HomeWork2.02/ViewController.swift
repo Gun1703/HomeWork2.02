5835aa8b327dc9f7c66ceab498bdc6f110d7fc8b
Gun <ggunin1992@gmail.com>
2023-01-30 19:22:30 +0300

    Initial Commit

---
+++ HomeWork2.02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2.02
+//
+//  Created by 1234 on 30.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
